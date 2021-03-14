8f7ccd0eef74be78f176ae1e0a4431455ffde7f7
dijinx <dimajinx@gmail.com>
2021-03-14 13:45:25 +0700

    добавил округление

--- src/test/java/ru/geekbrains/TriangleTest.java
+++ src/test/java/ru/geekbrains/TriangleTest.java
@@ -4,6 +4,8 @@ import im.TriangleSquare;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.text.DecimalFormat;
+
 
 public class TriangleTest {
 
@@ -12,7 +14,9 @@ public class TriangleTest {
 
         System.out.println("Test Start!");
         double res = TriangleSquare.geroneSquare(14,12,12);
-        Assertions.assertEquals(res, 68.23 );
+        DecimalFormat decimalFormat = new DecimalFormat("##.##");
+        String res2 = decimalFormat.format(res);
+        Assertions.assertEquals(res2, "68,23" );
         System.out.println("Test STOP!");
     }
 }
