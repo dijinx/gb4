f29f4334adf439259f4608e9fc318f7767bb7ee0
dijinx <dimajinx@gmail.com>
2021-03-14 13:55:03 +0700

    добавил округление

--- src/test/java/ru/geekbrains/TriangleTest.java
+++ src/test/java/ru/geekbrains/TriangleTest.java
@@ -13,10 +13,13 @@ public class TriangleTest {
     public void TestSimple() {
 
         System.out.println("Test Start!");
-        double res = TriangleSquare.geroneSquare(14,12,12);
+        double res = TriangleSquare.geroneSquare(14, 12, 12);
         DecimalFormat decimalFormat = new DecimalFormat("##.##");
         String res2 = decimalFormat.format(res);
-        Assertions.assertEquals(res2, "68,23" );
+        Assertions.assertEquals(res2, "68,23");
+        if (res2.equals("68,23"))
+            System.out.println("Ожидаемый результат: " + "68,23" + "\n" + "Фактический результат: " + res2 + "\n" + "Test passed!");
+        else System.out.println("Test failed!");
         System.out.println("Test STOP!");
     }
 }
